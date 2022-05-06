49fec518b4a1a05276eb1a3b6272c89ac6a07071
James Gitchell <jamesgitchell.dev@gmail.com>
2022-05-05 20:11:43 -0400

    dom fixes

---
+++ src/components/Hero.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default function Hero(){
+    return(
+        <section className="hero">
+           <img src="../images/photo-grid.png" className="main--img"   />
+           <h1>Online Experiences</h1>
+           <p className="main--info">Join unique interactive activities lead by one-of-a-kind hosts--all without leaving home</p>
+        </section>
+    )
+}
